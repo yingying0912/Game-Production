6465175960731080feb24ec91ce3f559ca82662c
yingying0912 <xiiaoying5201314@gmail.com>
2020-08-24 21:48:08 +0800

    V0.0.2
    Switch Scenes

---
+++ WatchOut/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+    private bool isLookedAt = false;
+    public float timerDuration = 2f;
+    public float lookTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLookedAt)
+        {
+            lookTimer += Time.deltaTime;
+
+
+            if (lookTimer > timerDuration)
+            {
+                lookTimer = 0f;
+                Debug.Log("Button selected!");
+                GetComponent<Button>().onClick.Invoke();
+            }
+        }
+        else
+        {
+            lookTimer = 0f;
+        }
+    }
+
+    public void SetGazeAt(bool gazedAt)
+    {
+        isLookedAt = gazedAt;
+    }
+
+}
---
+++ WatchOut/Assets/Scripts/SceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public int level;
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadPrevScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGameLevel(int level)
+    {
+        SceneManager.LoadScene(level + 2);
+    }
+
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
