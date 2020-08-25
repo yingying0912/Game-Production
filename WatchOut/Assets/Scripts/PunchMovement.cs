3fc1ff3d19e6bc4eaa26302d98ed32d1f941e2d3
yingying0912 <xiiaoying5201314@gmail.com>
2020-08-25 23:33:12 +0800

    V0.2
    Add tutorial Scence

--- WatchOut/Assets/Scripts/PunchMovement.cs
+++ WatchOut/Assets/Scripts/PunchMovement.cs
@@ -8,13 +8,14 @@ public class PunchMovement : MonoBehaviour
     public Rigidbody rg;
     public GameObject camera;
     public Vector3 velocity;
+    public int speed = 1;
 
     // Update is called once per frame
     void Start()
     {        
         velocity = camera.transform.position - transform.position;
         velocity.Normalize();
-        velocity *= 5;
+        velocity *= speed;
     }
 
     void Update()
